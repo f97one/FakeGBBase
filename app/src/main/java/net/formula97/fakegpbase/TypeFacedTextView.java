1bc00c6ec8fa126ed0852e7acc9ac82bd3066a87
HAJIME Fukuna <f97one@gmail.com>
2014-10-26 19:18:52 +0900

    カスタムTextViewの中身を実装。

--- app/src/main/java/net/formula97/fakegpbase/TypeFacedTextView.java
+++ app/src/main/java/net/formula97/fakegpbase/TypeFacedTextView.java
@@ -1,6 +1,8 @@
 package net.formula97.fakegpbase;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -9,17 +11,41 @@ import android.widget.TextView;
  */
 public class TypeFacedTextView extends TextView {
 
-    private String defaultFontName = "formation_sans_regular.ttf";
+    private String mFontName = "formation_sans_regular.ttf";
 
     public TypeFacedTextView(Context context) {
         super(context);
+        init();
     }
 
     public TypeFacedTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        getFont(context, attrs);
+        init();
     }
 
     public TypeFacedTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        getFont(context, attrs);
+        init();
+    }
+
+    public void getFont(Context context, AttributeSet attributeSet) {
+        if (isInEditMode()) {
+            return;
+        }
+
+        TypedArray ta = context.obtainStyledAttributes(attributeSet, R.styleable.TypeFacedTextView);
+        mFontName = ta.getString(R.styleable.TypeFacedTextView_font);
+        ta.recycle();
+    }
+
+    public void init() {
+        if (isInEditMode()) {
+            return;
+        }
+
+        Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), mFontName);
+        setTypeface(typeface);
     }
 }
