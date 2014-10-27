2c9742c72b7b6935ec890222ef1ebf0ee5c39da9
HAJIME Fukuna <f97one@gmail.com>
2014-10-27 22:56:09 +0900

    レイアウトのリソースを取得する処理を追加。

--- app/src/main/java/net/formula97/fakegpbase/MainActivity.java
+++ app/src/main/java/net/formula97/fakegpbase/MainActivity.java
@@ -4,14 +4,30 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    private TextView tvBuilderName;
+    private TextView tvFighterName;
+    private TextView tvScale;
+    private TextView tvClass;
+    private TextView tvModelNo;
+    private TextView tvGunplaName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvBuilderName = (TextView) findViewById(R.id.tvBuilderName);
+        tvFighterName = (TextView) findViewById(R.id.tvFighterName);
+        tvScale = (TextView) findViewById(R.id.tvScale);
+        tvClass = (TextView) findViewById(R.id.tvClass);
+        tvModelNo = (TextView) findViewById(R.id.tvModelNo);
+        tvGunplaName = (TextView) findViewById(R.id.tvGunplaName);
+
     }
 
 
