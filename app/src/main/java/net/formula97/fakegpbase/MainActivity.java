e66e2658f6d77664345f41f1c74356370dbd5fef
HAJIME Fukuna <f97one@gmail.com>
2014-10-30 00:33:39 +0900

    NFC機能の存在と有効か否かでDialogを表示する処理を追加。

--- app/src/main/java/net/formula97/fakegpbase/MainActivity.java
+++ app/src/main/java/net/formula97/fakegpbase/MainActivity.java
@@ -2,13 +2,18 @@ package net.formula97.fakegpbase;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import net.formula97.fakegpbase.fragments.MessageDialogs;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements MessageDialogs.DialogsButtonSelectionCallback {
 
     private TextView tvBuilderName;
     private TextView tvFighterName;
@@ -16,6 +21,8 @@ public class MainActivity extends Activity {
     private TextView tvClass;
     private TextView tvModelNo;
     private TextView tvGunplaName;
+
+    protected NfcAdapter mNfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,24 @@ public class MainActivity extends Activity {
         tvModelNo = (TextView) findViewById(R.id.tvModelNo);
         tvGunplaName = (TextView) findViewById(R.id.tvGunplaName);
 
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        // NFCアダプターが有効でない場合は、設定画面を呼ぶか否かをDialogを表示する
+        if (mNfcAdapter != null) {
+            if (!mNfcAdapter.isEnabled()) {
+                MessageDialogs dialogs = MessageDialogs.getInstance(
+                        getString(R.string.dialgo_info),
+                        getString(R.string.wish_to_enable),
+                        MessageDialogs.BUTTON_BOTH
+                );
+                dialogs.show(getFragmentManager(), MessageDialogs.FRAGMENT_TAG);
+            }
+        } else {
+            MessageDialogs dialogs = MessageDialogs.getInstance(
+                    getString(R.string.dialog_warn),
+                    getString(R.string.no_nfc_present),
+                    MessageDialogs.BUTTON_POSITIVE);
+            dialogs.show(getFragmentManager(), MessageDialogs.FRAGMENT_TAG);
+        }
     }
 
 
@@ -54,5 +79,18 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onButtonPressed(String messageBody, int which) {
+        if (messageBody.equals(getString(R.string.wish_to_enable)) &&
+                which == MessageDialogs.PRESSED_POSITIVE) {
+            // 設定画面をstartActivityする
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
+            } else {
+                startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+            }
+        }
     }
 }
