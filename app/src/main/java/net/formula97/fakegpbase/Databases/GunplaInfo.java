ddfda0e32497f71eb4d217fec75d675fcb0dd874
HAJIME Fukuna <f97one@gmail.com>
2014-11-02 21:33:49 +0900

    配置先パッケージを１階層深いところに移動。

--- app/src/main/java/net/formula97/fakegpbase/Databases/GunplaInfo.java
+++ app/src/main/java/net/formula97/fakegpbase/Databases/GunplaInfo.java
@@ -1,9 +1,7 @@
-package net.formula97.fakegpbase;
+package net.formula97.fakegpbase.Databases;
 
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
-
-import java.sql.Struct;
 
 /**
  * がんプラ情報を保持するDTO。
