7f77d1867cf2092630feb8cd582429a52ec6347f
HAJIME Fukuna <f97one@gmail.com>
2014-11-02 21:37:26 +0900

    配置先パッケージを１階層深いところに移動。

---
+++ app/src/main/java/net/formula97/fakegpbase/AppConst.java
@@ -0,0 +1,13 @@
+package net.formula97.fakegpbase;
+
+/**
+ * 雑多な定数値を格納するユーティリティクラス。<br />
+ * Created by f97one on 14/11/02.
+ */
+public class AppConst {
+
+    private AppConst() { }
+
+    public static final String DATABASE_NAME = "GunplaInfo.db";
+    public static final int DATABASE_VERSION = 1;
+}
