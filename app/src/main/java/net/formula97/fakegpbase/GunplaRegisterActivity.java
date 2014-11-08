998097c9a470775630949beedf015fd3aefbf74d
HAJIME Fukuna <f97one@gmail.com>
2014-11-08 10:04:25 +0900

    ウィジェット初期化とSpinner周りの処理を追加。

--- app/src/main/java/net/formula97/fakegpbase/GunplaRegisterActivity.java
+++ app/src/main/java/net/formula97/fakegpbase/GunplaRegisterActivity.java
@@ -1,10 +1,15 @@
 package net.formula97.fakegpbase;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -13,8 +18,17 @@ import android.widget.Spinner;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
+import net.formula97.fakegpbase.Databases.GunplaInfo;
+import net.formula97.fakegpbase.fragments.NewItemDialog;
 
-public class GunplaRegisterActivity extends Activity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class GunplaRegisterActivity extends Activity implements AdapterView.OnItemSelectedListener,
+        NewItemDialog.OnInputCompleteListener {
 
     private EditText etRegisterBuilderName;
     private EditText etRegisterFighterName;
@@ -28,11 +42,50 @@ public class GunplaRegisterActivity extends Activity {
     private EditText etRegisterGunplaName;
     private AdView adView2;
 
+    private String mBuiderName;
+    private String mFighterName;
+    private int mScaleSelectedPos;
+    private String mScaleVal;
+    private int mClassSelectedPos;
+    private String mClassVal;
+    private int mScratchSelected;
+    private String mModelName;
+    private String mGunplaName;
+
+    private GunplaInfo mGunplaInfo;
+    private ArrayAdapter<String> scaleAdapter;
+    private ArrayAdapter<String> classAapter;
+    private boolean isScaleSelected = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gunpla_register);
 
+        initView();
+
+        adView2 = (AdView) findViewById(R.id.adView2);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView2.loadAd(adRequest);
+
+        initVariables();
+
+        String[] scales = getResources().getStringArray(R.array.scale_list);
+        List<String> scaleList = new ArrayList<>(Arrays.asList(scales));
+        scaleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, scaleList);
+        scaleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerScaleName.setAdapter(scaleAdapter);
+        spinnerScaleName.setOnItemSelectedListener(this);
+
+        String[] classes = getResources().getStringArray(R.array.class_list);
+        List<String> classList = new ArrayList<>(Arrays.asList(classes));
+        classAapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, classList);
+        classAapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerClassName.setAdapter(classAapter);
+        spinnerClassName.setOnItemSelectedListener(this);
+    }
+
+    private void initView() {
         etRegisterBuilderName = (EditText) findViewById(R.id.etRegisterBuilderName);
         etRegisterFighterName = (EditText) findViewById(R.id.etRegisterFighterName);
         spinnerScaleName = (Spinner) findViewById(R.id.spinnerScaleName);
@@ -43,10 +96,26 @@ public class GunplaRegisterActivity extends Activity {
         radioBtnFullScratch = (RadioButton) findViewById(R.id.radioBtnFullScratch);
         etRegisterModelName = (EditText) findViewById(R.id.etRegisterModelName);
         etRegisterGunplaName = (EditText) findViewById(R.id.etRegisterGunplaName);
-        adView2 = (AdView) findViewById(R.id.adView2);
+    }
 
-        AdRequest adRequest = new AdRequest.Builder().build();
-        adView2.loadAd(adRequest);
+    /**
+     * 変数を初期化する。
+     */
+    private void initVariables() {
+        // ビルダー名とファイター名の初期値はPreferenceの値とする
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        mBuiderName = pref.getString(AppConst.PREF_KEY_BUILDER_NAME, "");
+        mFighterName = pref.getString(AppConst.PREF_KEY_FIGHTER_NAME, "");
+        mScaleSelectedPos = 0;
+        mScaleVal = "";
+        mClassSelectedPos = 0;
+        mClassVal = "";
+        mScratchSelected = 0;
+        mModelName = "";
+        mGunplaName = "";
+
+        mGunplaInfo = new GunplaInfo();
     }
 
 
@@ -77,6 +146,27 @@ public class GunplaRegisterActivity extends Activity {
         super.onResume();
 
         adView2.resume();
+
+        etRegisterBuilderName.setText(mBuiderName);
+        etRegisterFighterName.setText(mFighterName);
+        spinnerScaleName.setSelection(mScaleSelectedPos, false);
+        spinnerClassName.setSelection(mClassSelectedPos, false);
+        // RadioButtonは、先に全てオフにする
+        radioBtnNonScratch.setChecked(false);
+        radioBtnPartialScratch.setChecked(false);
+        radioBtnFullScratch.setChecked(false);
+        switch (mScratchSelected) {
+            case AppConst.NO_SCRATCH_BUILT:
+                radioBtnNonScratch.setChecked(true);
+                break;
+            case AppConst.PATIAL_SCRATCH_BUILT:
+                radioBtnPartialScratch.setChecked(true);
+                break;
+            case AppConst.FULL_SCRTATCH_BUILT:
+                radioBtnFullScratch.setChecked(true);
+        }
+        etRegisterModelName.setText(mModelName);
+        etRegisterGunplaName.setText(mGunplaName);
     }
 
     @Override
@@ -91,5 +181,45 @@ public class GunplaRegisterActivity extends Activity {
         adView2.destroy();
 
         super.onDestroy();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String selItem = (String) parent.getAdapter().getItem(position);
+
+        if (parent.getAdapter().equals(scaleAdapter)) {
+            mScaleSelectedPos = position;
+            mScaleVal = selItem;
+            isScaleSelected = true;
+        } else if (parent.getAdapter().equals(classAapter)) {
+            mClassSelectedPos = position;
+            mClassVal = selItem;
+            isScaleSelected = false;
+        }
+
+        if (selItem.equals(getString(R.string.other))) {
+            // 選択アイテムが「その他」だった場合は、入力ダイアログを表示する
+            NewItemDialog dialog = new NewItemDialog();
+            dialog.show(getFragmentManager(), NewItemDialog.FRAGMENT_TAG);
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onInputComplered(String inputItem) {
+        if (isScaleSelected) {
+            scaleAdapter.insert(inputItem, mScaleSelectedPos);
+            spinnerScaleName.setSelection(mScaleSelectedPos, false);
+            scaleAdapter.notifyDataSetChanged();
+        } else {
+            classAapter.insert(inputItem, mClassSelectedPos);
+            spinnerClassName.setSelection(mClassSelectedPos, false);
+            classAapter.notifyDataSetChanged();
+        }
     }
 }
---
+++ app/src/main/java/net/formula97/fakegpbase/fragments/NewItemDialog.java
@@ -0,0 +1,84 @@
+package net.formula97.fakegpbase.fragments;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+
+import net.formula97.fakegpbase.AppConst;
+import net.formula97.fakegpbase.R;
+
+import java.util.EventListener;
+
+/**
+ * Created by f97one on 14/11/08.
+ */
+public class NewItemDialog extends DialogFragment {
+
+    public static final String FRAGMENT_TAG = NewItemDialog.class.getName() + AppConst.DIALOG_FRAGMENT_SUFFIX;
+    public static final int EDITTEXT_ID = 0x7fff1001;
+
+    // アイテム入力終了を通知するイベントリスナ。
+    public interface OnInputCompleteListener extends EventListener {
+        /**
+         * アイテム入力終了後に呼び出される。
+         *
+         * @param inputItem 入力したアイテム
+         */
+        public void onInputComplered(String inputItem);
+    }
+
+    private OnInputCompleteListener mListener;
+
+    public NewItemDialog() {
+
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        if (activity instanceof OnInputCompleteListener) {
+            mListener = (OnInputCompleteListener) activity;
+        } else {
+            mListener = null;
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final EditText et = new EditText(getActivity());
+        et.setId(EDITTEXT_ID);
+        et.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    if (mListener != null) {
+                        mListener.onInputComplered(et.getText().toString());
+                    }
+                    getDialog().dismiss();
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(getString(R.string.input_new))
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        getDialog().dismiss();
+                    }
+                })
+                .setView(et);
+
+        return builder.create();
+    }
+}
