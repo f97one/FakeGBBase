781f43d8057f822691b498e601bb41a2efa5943d
HAJIME Fukuna <f97one@gmail.com>
2014-11-19 23:08:53 +0900

    EditTextPreferenceのサマリを、Preferenceの値で初期表示する処理を追加。

--- app/src/main/java/net/formula97/fakegpbase/SettingActivity.java
+++ app/src/main/java/net/formula97/fakegpbase/SettingActivity.java
@@ -1,12 +1,16 @@
 package net.formula97.fakegpbase;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
-
+/**
+ * Preference画面を処理するActivity。
+ */
 public class SettingActivity extends Activity {
 
     @Override
@@ -35,18 +39,31 @@ public class SettingActivity extends Activity {
 
             // 入力制限フィルタの定義
             // ※正確には入力制限ではないが、挙動的に入力制限になっている
-            EditTextPreference builderName = (EditTextPreference) findPreference("builder_name");
-            EditTextPreference fighterName = (EditTextPreference) findPreference("fighter_name");
+            EditTextPreference builderName = (EditTextPreference) findPreference(AppConst.PREF_KEY_BUILDER_NAME);
+            EditTextPreference fighterName = (EditTextPreference) findPreference(AppConst.PREF_KEY_FIGHTER_NAME);
             builderName.setOnPreferenceChangeListener(listener);
             fighterName.setOnPreferenceChangeListener(listener);
+
+            // EditTextPreferenceのサマリに、Preferenceの値を表示する
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String fname = pref.getString(AppConst.PREF_KEY_FIGHTER_NAME, "");
+            String bname = pref.getString(AppConst.PREF_KEY_BUILDER_NAME, "");
+            builderName.setSummary(bname);
+            fighterName.setSummary(fname);
+
+            // TODO ライセンス表示とFormationSans公開サイトへのリンクを用意する
         }
 
+        /**
+         * Preferenceの入力値が変更されたあとの処理を行うコールバックリスナ。<br />
+         * 正規表現でマッチした場合のみ、Preferenceの更新を許可する。
+         */
         private Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
                 String edited = newValue.toString();
 
-                // フィルタの正規表現にマッチした時だけOKと
+                // フィルタの正規表現にマッチした時だけOKとし、サマリを入力値で更新する
                 if (edited.matches(AppConst.INPUT_FILTER_ALL_CAPS_WITH_NUMBER)) {
                     preference.setSummary(edited);
                     return true;
