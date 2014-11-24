6cb9ef13a6dffb52fae0305db1420cb8545f643a
HAJIME Fukuna <f97one@gmail.com>
2014-11-24 19:42:05 +0900

    タグ不使用の場合のプリフィックスが仕様と違っていたのを修正。

--- app/src/main/java/net/formula97/fakegpbase/AppConst.java
+++ app/src/main/java/net/formula97/fakegpbase/AppConst.java
@@ -26,5 +26,5 @@ public class AppConst {
     public static final String INPUT_FILTER_ALL_CAPS_WITH_NUMBER = "^[A-Za-z0-9./ ]+$";
     public static final String INPUT_FILTER_ALL_CAPS_WITH_NUMBER_AND_SIMBOLS = "^[A-Za-z0-9./(){}*+, ]+$";
 
-    public static final String TAGLESS_TAG_ID_PREFIX = "NOTAG-";
+    public static final String TAGLESS_TAG_ID_PREFIX = "NOTAG_";
 }
